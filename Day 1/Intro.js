2f7222276764fa68606256a8fa3efa2efdf7a427
manu-hn <manuu.hn11@gmail.com>
2024-03-21 04:23:06 +0530

    Starting

---
+++ Day 1/Intro.js
@@ -0,0 +1,9 @@
+var UserAccount = /** @class */ (function () {
+    function UserAccount(name, email, mobile) {
+        this.name = name;
+        this.email = email;
+        this.mobile = mobile;
+    }
+    return UserAccount;
+}());
+var user = new UserAccount('Manu', "mail", 5);
